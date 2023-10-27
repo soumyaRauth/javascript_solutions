031f27df348ef4c70edc717110aadbba9e9d3171
Soumya Rauth <soumyaRauth@users.noreply.github.com>
2023-10-27 21:18:44 +0000

    Prefix and Postfix incremental operator

---
+++ 03_incremental_operator/test.js
@@ -0,0 +1,15 @@
+
+//* Postfix operator
+let x=0;
+
+let y=x++;
+
+console.log(`Postfix: x:${x} y:${y}`); //output will be x:1 y:0
+
+//* Prefix operator
+let x1=0;
+
+let y1=++x1;
+
+console.log(`Prefix: x1:${x} y1:${y1}`); //output will be x1:1 y1:1
+
